12d32ee9bc54ce49dad46ced3ab1b1a7c499a607
juasesan <jssa132@gmail.com>
2020-10-16 16:20:47 -0500

    Corrige un error en el archivo Printer1

--- app/src/main/java/com/example/a3dprint_control/Printer1.java
+++ app/src/main/java/com/example/a3dprint_control/Printer1.java
@@ -101,7 +101,7 @@ public class Printer1 extends Activity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
-                e.printStackTrace();
+                e.printStackTrace();}
       
 
         //FIN DEL CODIGO PARA OBTENER LAS RUTAS DE LOS ARCHIVOS
